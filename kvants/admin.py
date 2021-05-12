3d9a0ea0120139f48f802d94bc37ff82e1de0969
Sam <51040158+SememN@users.noreply.github.com>
2021-05-12 18:57:40 +0500

    register course and description models

--- kvants/admin.py
+++ kvants/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ItCourse, AutoCourse, AeroCourse, MathCourse, PromDesCourse, PromRoboCourse, HiTechCourse,\
+    ChessCourse, ItDescription, AutoDescription, AeroDescription, MathDescription, PromDesDescription,\
+    PromRoboDescription, HiTechDescription, ChessCDescription
+
+
+admin.site.register(ItCourse)
+admin.site.register(AutoCourse)
+admin.site.register(AeroCourse)
+admin.site.register(MathCourse)
+admin.site.register(PromDesCourse)
+admin.site.register(PromRoboCourse)
+admin.site.register(HiTechCourse)
+admin.site.register(ChessCourse)
+
+admin.site.register(ItDescription)
+admin.site.register(AutoDescription)
+admin.site.register(AeroDescription)
+admin.site.register(MathDescription)
+admin.site.register(PromDesDescription)
+admin.site.register(PromRoboDescription)
+admin.site.register(HiTechDescription)
+admin.site.register(ChessCDescription)
