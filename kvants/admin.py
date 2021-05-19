1c181233345d385d47f72b36cbd91b0f71ac647e
Sam <51040158+SememN@users.noreply.github.com>
2021-05-19 17:33:33 +0500

    del chess and maths

--- kvants/admin.py
+++ kvants/admin.py
@@ -1,24 +1,19 @@
 from django.contrib import admin
 
-from .models import ItCourse, AutoCourse, AeroCourse, MathCourse, PromDesCourse, PromRoboCourse, HiTechCourse,\
-    ChessCourse, ItDescription, AutoDescription, AeroDescription, MathDescription, PromDesDescription,\
-    PromRoboDescription, HiTechDescription, ChessCDescription
-
+from .models import ItCourse, AutoCourse, AeroCourse, PromDesCourse, PromRoboCourse, HiTechCourse,\
+    ItDescription, AutoDescription, AeroDescription, PromDesDescription,\
+    PromRoboDescription, HiTechDescription
 
 admin.site.register(ItCourse)
 admin.site.register(AutoCourse)
 admin.site.register(AeroCourse)
-admin.site.register(MathCourse)
 admin.site.register(PromDesCourse)
 admin.site.register(PromRoboCourse)
 admin.site.register(HiTechCourse)
-admin.site.register(ChessCourse)
 
 admin.site.register(ItDescription)
 admin.site.register(AutoDescription)
 admin.site.register(AeroDescription)
-admin.site.register(MathDescription)
 admin.site.register(PromDesDescription)
 admin.site.register(PromRoboDescription)
 admin.site.register(HiTechDescription)
-admin.site.register(ChessCDescription)
