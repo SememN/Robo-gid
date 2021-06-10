9e7e7f68b77b2aede88780018b0f38d7ff5e6f35
tovarishchvamtutdelatnechego <67748562+tovarishchvamtutdelatnechego@users.noreply.github.com>
2021-06-10 16:23:31 +0500

    Add files via upload

---
+++ trerror.py
@@ -0,0 +1,106 @@
+# Pygame шаблон - скелет для нового проекта Pygame
+import pygame
+import random
+
+WIDTH = 200
+HEIGHT = 650
+FPS = 30
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+class Button(pygame.sprite.Sprite):
+
+    def update(self):
+        print('s')
+
+class Up(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, (HEIGHT / 2) - 51)
+class Down(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+class Left(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = ((WIDTH / 2) - 51, HEIGHT / 2)
+class Right(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = ((WIDTH / 2) + 51, HEIGHT / 2)
+class Plus(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH - 51, 51)
+class Minus(Button):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (51, 51)
+
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+up = Up()
+all_sprites.add(up)
+down = Down()
+all_sprites.add(down)
+left = Left()
+all_sprites.add(left)
+right = Right()
+all_sprites.add(right)
+plus = Plus()
+all_sprites.add(plus)
+minus = Minus()
+all_sprites.add(minus)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+
+    # Рендеринг
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
