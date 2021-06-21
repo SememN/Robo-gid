ec77c2212e827bfdce83854686831219091e8e6c
Sam <51040158+SememN@users.noreply.github.com>
2021-06-21 15:24:22 +0500

    Update models.py

--- kvant_projects/models.py
+++ kvant_projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=60)
+    preview = models.TextField(max_length=300)
+    description = models.TextField(max_length=5000)
+    teacher = models.TextField(max_length=60)
