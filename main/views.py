76efb23568f51280dd3831774556f9d39cfe30b5
Sam <51040158+SememN@users.noreply.github.com>
2021-06-21 15:26:03 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from kvant_projects import services
+
 
 def load_main_page(request):
-    return render(request, 'main_page/main.html')
+    return render(request, 'main_page/main.html', {'projects': services.get_projects()},)
